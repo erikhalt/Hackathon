93392d33f29131797d2b291a5259af819feae99d
Šimon Maňour <manousim@fel.cvut.cz>
2024-09-13 23:58:44 +0200

    denoised csv

--- simon/denoise.py
+++ simon/denoise.py
@@ -10,6 +10,7 @@ def main():
     df = pd.read_csv(P_DATABASE, index_col=0, header=None)
     df = df.interpolate(method='linear')
 
+    df = df.interpolate(method='linear', axis=1)
     df.to_csv(P_OUT, header=False)
 
 
