167535f41558b2fe7387d21f9d7e4a572d7026d5
erikhalt <erikhalt@hotmail.com>
2024-09-14 16:20:54 +0200

    aa

--- formula_usage/plot.py
+++ formula_usage/plot.py
@@ -1,13 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-class plot:
-    def __init__(self):
-        dfs = []
-
-    def setup(self,datasets: list):
-        for x in datasets:
-            new_df = pd.read_csv(x)
-            self.dfs.appen(new_df)
-
-
-        
+def plot(csv):
+    csv.plot(y=['time'])
+    plt.show()
